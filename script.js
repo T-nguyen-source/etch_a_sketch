ec455c46d4dd55c03aa2fce7b83287cdd250e207
T-nguyen-source <thomasnguyenmsw@GMAIL.COM>
2023-08-16 21:13:52 -0500

    CSS design to make it more visually appealing

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-window.onload = function() { 
-    // Draw the initial 16x16 grid
-    drawGrid(16);
-}
-
+// Function to draw the grid based on the given size
 function drawGrid(size) {
     const container = document.getElementById('container');
-
-    // Reset the container
     container.innerHTML = '';
-
-    // Calculate the size of each square
     const squareSize = 960 / size; // 960px is the width of our container
 
     for (let i = 0; i < size * size; i++) {
@@ -17,16 +9,55 @@ function drawGrid(size) {
         square.classList.add('square');
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
+        square.dataset.interactions = '0'; // Data attribute to track interactions
 
         // Add hover listener
-        square.addEventListener('mouseover', function() {
-            this.classList.add('hovered');
-        });
-
+        square.addEventListener('mouseover', changeColor);
+        
         container.appendChild(square);
     }
 }
 
+// Function to change color and darken progressively
+function changeColor() {
+    // Retrieve the number of interactions with this square
+    let interactions = parseInt(this.dataset.interactions);
+
+    // Generate random RGB values
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    // Compute the darkening effect. 10% per interaction, up to 100%
+    let darkeningEffect = Math.min(interactions * 10, 100);
+    
+    // Apply darkening effect to RGB values
+    red = Math.floor(red * (1 - darkeningEffect / 100));
+    green = Math.floor(green * (1 - darkeningEffect / 100));
+    blue = Math.floor(blue * (1 - darkeningEffect / 100));
+
+    // Set the new color of the square
+    this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    
+    // Increment the interactions count for this square
+    this.dataset.interactions = interactions + 1;
+}
+
+// Function to clear the grid
+function clearGrid() {
+    const container = document.getElementById('container');
+    
+    // Convert live HTMLCollection to a static array
+    const squares = Array.from(container.getElementsByClassName('square'));
+
+    // Iterate over each square and reset its properties
+    for (let square of squares) {
+        square.style.backgroundColor = '';
+        square.dataset.interactions = '0';
+    }
+}
+
+// Function to create a new grid based on user input
 function newGrid() {
     let size = prompt("Enter the number of squares per side:", "16");
 
@@ -38,4 +69,9 @@ function newGrid() {
     }
 
     drawGrid(size);
+}
+
+// Draw the initial 16x16 grid when the page loads
+window.onload = function() { 
+    drawGrid(16);
 }
